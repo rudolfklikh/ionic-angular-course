3cb33dbc7b830154049dde815e1890bbe56f1a54
unknown <rudolfklikh@gmail.com>
2021-03-16 14:53:10 +0100

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  text = 'Default starting text';
+
+  onChangeText() {
+    this.text = 'Changed';
+  }
 
 }
